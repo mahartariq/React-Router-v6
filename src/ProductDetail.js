f3457500ef4c412a2c4db1a14da02eee44176a19
mahartariq <mahartariq499@gmail.com>
2021-01-02 23:49:18 +0500

    change

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -6,6 +6,9 @@ import { useParams } from 'react-router-dom';
     return (
         <div>
             <h1>Fashion Tonight Product:{productID}</h1>
+            <h2>Sony</h2>
+            <h2>Apple</h2>
+            <h2>Samsung</h2>
         </div>
     )
 }
